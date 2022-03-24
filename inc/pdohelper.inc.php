e922a720b2f4a356ca83a1819ea2d0cbc28655eb
numeristacom <51783404+numeristacom@users.noreply.github.com>
2022-03-24 23:48:38 +0100

    Adding data dic support

--- inc/pdohelper.inc.php
+++ inc/pdohelper.inc.php
@@ -19,7 +19,7 @@
 */
 
 /**
- * MariaDB wrapper. This class wraps the PHP  functions, integrating error control and
+ * MariaDB wrapper. This class wraps the PHP functions, integrating error control and
  * reporting through the static debug class and allowing handling errors through the debug class
  * in a consistant way.
  * @package RedSea
@@ -135,6 +135,10 @@ class pdodb {
            }
         }
     }
+
+    public function getDBConnection() {
+        return $this->dbConnection;
+    }
 }
 
 /**
@@ -192,5 +196,132 @@ class recordset {
         }
     }
 }
+
+class singleTableRecord {
+
+    /**
+     * Contains the database connection object
+     * @internal 
+     */
+    protected $dbConnection = null;
+    protected $tableSetup = array();
+    protected $whereArgs = array();
+    private $coherencyCheck = true;
+    protected $tableName = null;
+
+    /**
+     * Set up the object by passing it a valid pdo connection object, either directly created or exported from the pdodb class.
+     * @param mixed $pdoConnectionObject 
+     * @param string $tableToWork The name of the database table to work on.
+     * @return void 
+     */
+    public function __construct($pdoConnectionObject, $tableToWork) 
+    {
+        $this->dbConnection = $pdoConnectionObject;
+        $this->tableName = str_replace("'", "''", str_replace("''", "'", $tableToWork));
+
+        $ret = $this->dbConnection->query("SHOW FULL COLUMNS FROM " . $this->tableName);
+        //The query will return an object if it ran. Anything else is a problem.
+        if(!is_object($ret)) {
+            debug::err($this->dbConnection->errorInfo(), $this->tableName);
+        } else {
+            //Lets build the handling array.
+            while ($result = $ret->fetch(PDO::FETCH_ASSOC)) {
+                $fieldDicDesc = explode('|', $result['Comment']);
+                $currentField = $result['Field'];
+                // Set the coherency check flag to false if we miss any data in the description.
+                if(count($fieldDicDesc) < 3) {
+                    $this->coherencyCheck = false;
+                    debug::err("Less than 3 fields for $tableToWork.$currentField - Missing description data");
+                    return false;
+                } else {
+                    $enumArray = array();
+                    if($fieldDicDesc['1'] == "e") {
+                        //We have an enum field to customise.
+                        $enumData = explode("&", $fieldDicDesc[2]);
+                        foreach($enumData as $enumValue) {
+                            $ev = explode('=', $enumValue);
+                            $enumArray[$ev[0]] = $ev[1];
+                        }
+                    }
+                    $this->tableSetup[$currentField] = array("value" => null, "dataType" => $fieldDicDesc[0], "dataFormat" => $fieldDicDesc[1], "enumArray" => $enumArray);
+                }
+            }
+        }
+    }
+
+    /**
+     * Add the where conditions to an internal structure which will be used to generate a where query for reading or writing a table record.
+     * @param mixed $field Field in the current table to filter on
+     * @param mixed $value Value to use for the clause.
+     * @return void 
+     */
+    public function addWhere($field, $value) {
+        $field = $this->forceCleanValue('str', $field);
+        if(array_key_exists($field, $this->tableSetup)) {
+            $this->whereArgs[$field] = $this->forceCleanValue($this->tableName[$field]["dataType"], $value);
+        }
+    }
+
+    /**
+     * Return a query ready value that depends on the expected data type of the field. 
+     * @param mixed $expectedType num for Numerical fields (including boolean data), or str for String or text data.
+     * @param mixed $value Value to check
+     * @param bool $preQuote If true, any string value will be returned pre-quote delimited for use in a query in addition to being escaped, for example if you
+     * have a value abc'def passed as the value, then the method will return this value quoted and escaped as 'abc''def'. If false the string will only be escaped
+     * but will not be quoted.
+     * @return mixed Processed value
+     */
+    private function forceCleanValue($expectedType, $value, $preQuote = true) {
+        if($expectedType == "num") {
+            if(!is_numeric($value)) {
+                return null;
+            } else {
+                return $value;
+            }
+        } else {
+            if($preQuote) {
+                $quoteChar = "'";
+            } else {
+                $quoteChar = null;
+            }
+            return $quoteChar . str_replace("'", "''", str_replace("''", "'", $value)) . $quoteChar;
+        }
+    }
+
+    /**
+     * read a record from the table
+     * @return void 
+     */
+    public function readTable() {
+        $sql = "SELECT * FROM " . $this->tableName . " WHERE";
+        $i = 0;
+        foreach($this->whereArgs as $field => $value) {
+            if($i == 0) {
+                $sql .= " $field = $value";
+            } else {
+                $sql .= " AND $field = $value";
+            }
+            $i++;
+        }
+        $ret = $this->dbConnection->query($sql);
+        if(!is_object($ret)) {
+            debug::err($this->dbConnection->errorInfo(), $sql);
+        } else {
+            //Lets build the handling array.
+            while ($result = $ret->fetch(PDO::FETCH_ASSOC)) {
+               foreach($this->tableSetup as $col => $settings) {
+                $this->tableSetup[$col]["value"] = $result[$col];
+               }
+            }            
+        }
+    }
+
+    public function setValue($keyToSet, $value) {
+        
+    }
+
+}
+
 
 ?>
