2ada730e6c5543124206c697270bfa9454b62603
numeristacom <51783404+numeristacom@users.noreply.github.com>
2022-06-16 00:06:49 +0200

    update to js helper

--- inc/jshelper.js
+++ inc/jshelper.js
@@ -1,18 +1,39 @@
-// JS form helper functions
 
+function makeShortPseudoRandom() {
+    rc = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789";
+    dt = new Date()
+    mo = dt.getMonth()
+    dy = dt.getDay()
+    hr = dt.getHours(); 
+    mi = dt.getMinutes();
+    se = dt.getSeconds();
+    
+    pr = rc.substring(mo, mo+1) + rc.substring(dy, dy+1) + rc.substring(hr, hr+1) + rc.substring(mi, mi+1) + rc.substring(se, se+1)
 
-//Get the value of a selected element in a list.
+    for(let i = 1; i <= 5; i++) {
+        r = Math.floor(Math.random() * 61);
+        pr +=  rc.substring(r, r+1);
+    }
+    return pr;
+}
+
 function getSelectedValue(elementId) {
     return document.getElementById(elementId).options[document.getElementById(elementId).selectedIndex].value;
 }
 
-//Get the text of a selected element in a list
 function getSelectedText(elementId) {
     return document.getElementById(elementId).options[document.getElementById(elementId).selectedIndex].text;
 }
 
-//Add a new row to list
-function addElement(elementId, elementValue, elementDisplayText, elementDisabled=false) {
+function removeSelectedOption(elementId) {
+    idx = getSelectedIndex(elementId);
+
+    if(idx != null) {
+        document.getElementById(elementId).remove(idx);
+    }
+}
+
+function quickAddOption(elementId, elementValue, elementDisplayText, elementDisabled=false) {
     newOption = document.createElement('option');
     newOption.value = elementValue.toString();
     newOption.text = elementDisplayText.toString();
@@ -22,20 +43,59 @@ function addElement(elementId, elementValue, elementDisplayText, elementDisabled
     document.getElementById(elementId).add(newOption);
 }
 
-//Remove the selected element in a list
-function removeSelectedElement(elementId) {
-    idx = getSelectedIndex(elementId);
-    if(idx != null) {
-        document.getElementById(elementId).remove(idx);
-    }
+function setDivText(elementId, txtString) {
+    document.getElementById(elementId).innerHTML = txtString;
 }
 
-//Get the text from an element containing a value like a textbox
 function getText(elementId) {
     return document.getElementById(elementId).value;
 }
 
-//Get the index of a selected element in a listbox
 function getSelectedIndex(elementId) {
     return document.getElementById(elementId).selectedIndex;
 }
+
+function hideElement(elementId) {
+    document.getElementById(elementId).style.display = "none";
+}
+
+function showElement(elementId) {
+    document.getElementById(elementId).style.display = "block";
+}
+
+function getTimestamp() {
+    currentDate = new Date();
+    return currentDate.getTime();
+}
+
+function isChecked(elementId) {
+    if(document.getElementById(elementId).checked==true) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function setRBCB(elementId) {
+    document.getElementById(elementId).checked=true;
+}
+
+function unsetRBCB(elementId) {
+    document.getElementById(elementId).checked=false;
+}
+
+function selectedRB(radiobuttonGroupName) {
+    var getSelectedValue = document.querySelector( 'input[name="' + radiobuttonGroupName + '"]:checked');
+    if(getSelectedValue != null) {
+        return getSelectedValue.value;
+    } else {
+        return null;
+    }
+}
+
+function selectAllListBoxElements(elementId) {
+    var listbox = document.getElementById(elementId);
+    for (var count = 0; count < listbox.options.length; count++) {
+      listbox.options[count].selected = true;
+    }
+  }
